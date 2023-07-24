b6c87ed49c0f70b54db24d8063cebc1e2b081b76
Lucas Ricardo <lucasricardobarb@gmail.com>
2023-07-24 19:11:56 -0300

    test commit

--- script.js
+++ script.js
@@ -100,7 +100,7 @@ const Utils = {
     },
 
     formatAmount(value) {
-        value = Number(value) * 100
+        value = Number(value.replace(/\,\./g, "")) * 100
         return value
     },
 
